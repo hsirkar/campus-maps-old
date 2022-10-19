71ed92c6924a26f10896b8dfb213f80bb150246d
Rakrish Dhakal <dhakal.rakrish@gmail.com>
2022-10-19 16:14:30 -0400

    run prettier

--- packages/cm-mobile/babel.config.js
+++ packages/cm-mobile/babel.config.js
@@ -1,6 +1,6 @@
-export default function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-  };
+export default function (api) {
+    api.cache(true);
+    return {
+        presets: ['babel-preset-expo'],
+    };
 }
