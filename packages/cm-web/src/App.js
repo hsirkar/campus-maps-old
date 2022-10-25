9248fc03fb8e1d65c9434f4ad7a6559cbc927676
Rakrish Dhakal <dhakal.rakrish@gmail.com>
2022-10-25 04:16:55 -0400

    Run prettier

--- packages/cm-web/src/App.js
+++ packages/cm-web/src/App.js
@@ -3,7 +3,8 @@ import Map, { Marker } from 'react-map-gl';
 
 import 'mapbox-gl/dist/mapbox-gl.css';
 
-const MAPBOX_TOKEN = 'pk.eyJ1IjoicmFrcmlzaCIsImEiOiJjamptczYxOGMzc3dzM3BvbDB0andscXdwIn0.GY-HcAV_MakM6gwzSS17Fg'; // Set your mapbox token here
+const MAPBOX_TOKEN =
+    'pk.eyJ1IjoicmFrcmlzaCIsImEiOiJjamptczYxOGMzc3dzM3BvbDB0andscXdwIn0.GY-HcAV_MakM6gwzSS17Fg'; // Set your mapbox token here
 
 function App() {
     return (
@@ -11,11 +12,11 @@ function App() {
             initialViewState={{
                 latitude: 38.9869,
                 longitude: -76.9426,
-                zoom: 15
+                zoom: 15,
             }}
             minZoom={15}
             maxZoom={17}
-            style={{ width: "100vw", height: "100vh" }}
+            style={{ width: '100vw', height: '100vh' }}
             // mapStyle="mapbox://styles/rakrish/cl9hhahrr000q15khpnaawbex"
             mapStyle="mapbox://styles/mapbox/streets-v11"
             mapboxAccessToken={MAPBOX_TOKEN}>
