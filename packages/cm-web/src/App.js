2cf1c0c5a2b8b2b362d1bc95c3a8726946cec4d3
Rakrish Dhakal <dhakal.rakrish@gmail.com>
2022-10-25 04:11:53 -0400

    Add mapbox and bootstrap

--- packages/cm-web/src/App.js
+++ packages/cm-web/src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import Map, { Marker } from 'react-map-gl';
+
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+const MAPBOX_TOKEN = 'pk.eyJ1IjoicmFrcmlzaCIsImEiOiJjamptczYxOGMzc3dzM3BvbDB0andscXdwIn0.GY-HcAV_MakM6gwzSS17Fg'; // Set your mapbox token here
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer">
-                    Learn React
-                </a>
-            </header>
-        </div>
+        <Map
+            initialViewState={{
+                latitude: 38.9869,
+                longitude: -76.9426,
+                zoom: 15
+            }}
+            minZoom={15}
+            maxZoom={17}
+            style={{ width: "100vw", height: "100vh" }}
+            // mapStyle="mapbox://styles/rakrish/cl9hhahrr000q15khpnaawbex"
+            mapStyle="mapbox://styles/mapbox/streets-v11"
+            mapboxAccessToken={MAPBOX_TOKEN}>
+            <Marker latitude={38.9869} longitude={-76.9426} color="red" />
+        </Map>
     );
 }
 
