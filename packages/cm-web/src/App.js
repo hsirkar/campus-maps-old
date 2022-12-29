a4fdce8bf52f8d04fe47e992051bdded51a731bc
Rakrish Dhakal <dhakal.rakrish@gmail.com>
2022-12-29 13:46:07 -0500

    Add firebase WIP

--- packages/cm-web/src/App.js
+++ packages/cm-web/src/App.js
@@ -10,17 +10,45 @@ import Map from './components/Map';
 
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
+
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+
 dayjs.extend(relativeTime);
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBNYzyZz9QTpTZpTWBYyA9YZVpKddYN5zs",
+    authDomain: "campus-maps-53c7c.firebaseapp.com",
+    projectId: "campus-maps-53c7c",
+    storageBucket: "campus-maps-53c7c.appspot.com",
+    messagingSenderId: "591540730553",
+    appId: "1:591540730553:web:ca62a4bcf1d987ed753968",
+    measurementId: "G-X0QNYZD797"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+  
 
 const MAPBOX_TOKEN =
     'pk.eyJ1IjoicmFrcmlzaCIsImEiOiJjamptczYxOGMzc3dzM3BvbDB0andscXdwIn0.GY-HcAV_MakM6gwzSS17Fg';
 
 function App() {
     const mapRef = React.useRef();
-    const [data] = React.useState([]);
+    const [data, setData] = React.useState([]);
     const [selected, setSelected] = React.useState(-1);
 
+    function fetchData() {
+        fetch('https://localhost:3004/posts')
+            .then(res => res.json())
+            .then(json => {
+                setData(json);
+            });
+    }
+
     React.useEffect(() => {
+        fetchData();
         if (selected !== -1) {
             mapRef.current.easeTo({
                 center: {
